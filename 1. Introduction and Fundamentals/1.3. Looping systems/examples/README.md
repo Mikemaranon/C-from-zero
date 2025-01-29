3ec318516103eb13822a3b47eaf0f74577e2e6d7
Mikemaranon <mikemaranonx@gmail.com>
2025-01-29 14:23:38 +0100

    1.3

---
+++ 1. Introduction and Fundamentals/1.3. Looping systems/examples/README.md
@@ -0,0 +1,14 @@
+## Loop Exercises in C
+
+## example 1: Factorial Calculation
+Create a C program that computes the factorial of a given number using a `while` loop. The program should ask the user to input a positive integer and then output the factorial of that number.
+
+## example 2: Multiplication Table
+Develop a C program that prints the multiplication table of a given number using a `do-while` loop. The user should be able to input the number for which the multiplication table will be generated, and the program should display the table up to 10 times the input number.
+
+## example 3: Sum of Natural Numbers
+Write a C program to calculate the sum of the first `n` natural numbers using a `for` loop. The program should prompt the user to enter the value of `n` and then display the sum.
+
+## example 4: updated calculator
+Taking the calculator of `module 2.1`, set a system in where the user is set in a loop where he is asked if he want to use the calculator again after an operation.
+
---
+++ 1. Introduction and Fundamentals/1.3. Looping systems/examples/calculator.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, option;
+    float result;
+    char choice;
+
+    while(1==1) {
+        printf("Enter first number: ");
+        scanf("%d", &num1);
+        printf("Enter second number: ");
+        scanf("%d", &num2);
+
+        printf("=====================\n");
+        printf("|   1. add          |\n");
+        printf("|   2. subtract     |\n");
+        printf("|   3. multiply     |\n");
+        printf("|   4. divide       |\n");
+        printf("=====================\n");
+        printf("Select an option: ");
+        scanf("%d", &option);
+
+        switch (option) {
+            case 1:
+                result = num1 + num2;
+                printf("Result: %d + %d = %.2f\n", num1, num2, result);
+                break;
+            case 2:
+                result = num1 - num2;
+                printf("Result: %d - %d = %.2f\n", num1, num2, result);
+                break;
+            case 3:
+                result = num1 * num2;
+                printf("Result: %d * %d = %.2f\n", num1, num2, result);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = (float)num1 / num2;
+                    printf("Result: %d / %d = %.2f\n", num1, num2, result);
+                } else {
+                    printf("Error: Division by zero is not allowed.\n");
+                }
+                break;
+            default:
+                printf("Invalid option selected.\n");
+                break;
+        }
+        printf("Do you want to continue? (y: Yes, n: No): ");
+        scanf(" %c", &choice);
+        if(choice == 'n') {
+            break;
+        }
+    }
+    return 0;
+}
